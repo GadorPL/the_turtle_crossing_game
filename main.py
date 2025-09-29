0ed1c4782211505331c2f07234851b4f9211897b
Michał Hewelt <mhewelt9@gmail.com>
2025-09-29 18:29:47 +0200

    create player behavior

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+from turtle import Screen, Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.up, "Up")
+screen.onkeypress(player.down, "Down")
+
+
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
